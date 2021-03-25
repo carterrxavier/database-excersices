49c4ef3f5eb8e81d84eddfcb01c8992524854580
carterrxavier <carter.r.xavier@gmail.com>
2021-03-25 11:49:34 -0500

    order_by_exercises added to github

---
+++ order_by_exercises.sql
@@ -0,0 +1,42 @@
+use employees;
+
+#2 --first person = Irena Reutenauer, last person Vidya Simmen
+select first_name, last_name
+from employees 
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+order by first_name asc;
+
+#3 --first peron = Irena Acton , last person Vidya Zweizig
+select first_name, last_name
+from employees 
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+order by first_name asc, last_name asc;
+
+
+#4 --first person = Irena Acton , last person  Maya Zyda
+select first_name, last_name
+from employees 
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+order by last_name asc, first_name asc;
+
+#5 --first person = Ramzi Erde , last person  Dharmaraja Ertl
+select first_name, last_name, emp_no
+from employees 
+where last_name like 'e%'
+order by emp_no asc;
+
+
+#6 --362 eployees returned . first person = Teiji Eldridge , last person Sergi Erde
+select first_name, last_name, hire_date
+from employees 
+where last_name like 'e%' and last_name like '%e'
+order by hire_date desc;
+
+
+#7 -- oldest employee hired last = Khun Bernini,   Youngest employee hired first = Douadi Pettis
+select first_name, last_name, birth_date, hire_date
+from employees
+where birth_date like '%-12-25' and hire_date like '199%'
+order by `birth_date` asc, hire_date desc;
+
+
