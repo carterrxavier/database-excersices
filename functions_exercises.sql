d61c2d4d91c2b649835924df063951cce46e1546
carterrxavier <carter.r.xavier@gmail.com>
2021-03-29 13:39:24 -0500

    made changes to #3 and #4

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,12 +4,12 @@ use employees;
 #2
 select concat(first_name, ' ', last_name) as 'Full_name'
 from employees
-where last_name like '%e';
+where last_name like 'e%e' ;
 
 #3
 select upper(concat(first_name, ' ', last_name)) as 'Full_name'
 from employees
-where last_name like '%e';
+where last_name like 'e%e';
 
 #4
 select concat(first_name , ' ', last_name)as 'full_name', birth_date, hire_date, datediff(curdate(),hire_date) as 'Time past since hire date'
@@ -19,7 +19,6 @@ where hire_date like '199%' and birth_date like '%-12-25';
 #5
 select max(salary), min(salary)
 from salaries;
-
 #6
 select concat(lower(substr(first_name, 1, 1)), lower(substr(last_name, 1,4)), '_', substr(birth_date, 6,2), substr(birth_date, 3,2)) as username , first_name, last_name, birth_date
 from employees
